d116aba0868a854ecaeb8db403646ef3527740cb
Angelo <gabriel_propio95@hotmail.com>
2024-06-04 20:30:59 -0400

    añadido middleware para forzar https y agregado al archivo kernel.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,8 +60,6 @@
             @yield('content')
         </main>
     </div>
-     <!-- Scripts -->
-     @vite(['resources/js/app.js'])
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     
