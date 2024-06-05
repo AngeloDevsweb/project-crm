9daccddbd76fe71a96963b3c7bd44ca261f8eef7
Angelo <gabriel_propio95@hotmail.com>
2024-06-04 20:53:14 -0400

    agregando bootstrap inline para intentar solucionar error de estilos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,6 +10,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>crm.dev</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ secure_asset('css/home.css') }}">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
           <!-- Scripts -->
              @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
